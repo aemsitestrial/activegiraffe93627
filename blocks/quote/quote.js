7e92688c97f42999a44531edcd14ac500470bbcb
Pinaki Bandyopadhyay <pinaki@Pinakis-MacBook-Air.local>
2026-04-01 17:00:21 +0530

    feat: eslint issue fix try 2

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,7 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
-  // eslint-disable-next-line quotes
-  const blockquote = document.createElement("blockquote");
+  const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
 }
